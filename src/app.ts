365656e4380089048c345503e266f16eca9fb8eb
rakibulhasanroki <inbox.roki@gmail.com>
2026-01-27 17:02:43 +0600

    initialize backend with Express, TypeScript, Prisma (NeonDB) and modular folder structure

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send(`MediStore Backend Running`);
+});
+
+export default app;
